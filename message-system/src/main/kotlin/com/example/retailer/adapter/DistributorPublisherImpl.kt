0d1ab2c76c2052141a8ce9b9d62752970ee218cb
Anton Fedorov <avfedorov235@gmail.com>
2021-11-26 05:24:31 +0300

    +mq

---
+++ message-system/src/main/kotlin/com/example/retailer/adapter/DistributorPublisherImpl.kt
@@ -0,0 +1,30 @@
+package com.example.retailer.adapter
+
+import com.example.retailer.api.distributor.Order
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.amqp.core.TopicExchange
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Autowired
+
+class DistributorPublisherImpl: DistributorPublisher {
+    @Autowired
+    private lateinit var template: RabbitTemplate
+    @Autowired
+    private lateinit var topic: TopicExchange
+    private val mapper = jacksonObjectMapper()
+
+    override fun placeOrder(order: Order): Boolean {
+        val orderJson = mapper.writeValueAsString(order)
+        return if (order.id != null) {
+            template.convertAndSend(topic.name, "distributor.placeOrder.BOBRO.${order.id}", orderJson) {
+                it.messageProperties.headers["Notify-Exchange"] = "distributor_exchange"
+                it.messageProperties.headers["Notify-RoutingKey"] = "retailer.BOBRO"
+                it
+            }
+            true
+        } else {
+            println("order id == null !")
+            false
+        }
+    }
+}
--- message-system/src/main/kotlin/com/example/retailer/api/distributor/Order.kt
+++ message-system/src/main/kotlin/com/example/retailer/api/distributor/Order.kt
@@ -1,26 +1,45 @@
 package com.example.retailer.api.distributor
 
+import org.hibernate.annotations.GenericGenerator
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+import javax.persistence.GeneratedValue
+import javax.persistence.OneToMany
+import javax.persistence.CascadeType
+
 /**
  * Описание заказа
  */
+@Entity
+@Table(name = "orders")
 data class Order(
     /**
      * Уникальный идентификатор заказа на стороне ретейлера
      */
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid")
+    @Column(columnDefinition = "CHAR(32)")
     val id: String?,
 
     /**
      * Произвольный адрес доставки
      */
+    @Column
     val address: String,
 
     /**
      * Произвольный получатель доставки
      */
+    @Column
     val recipient: String,
 
     /**
      * Список заказанных товаров
      */
+//    @OneToMany(cascade = [CascadeType.ALL])
+    @OneToMany(cascade = [CascadeType.ALL])
     val items: List<Item>
 )
